f275cf01ffcde4bc2767c671329e549956e60321
luutien18195 <30953080+luutien18195@users.noreply.github.com>
2022-06-13 00:41:26 +0700

    update form scenario list

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<HomePage />}>
+          <Route path='/' element={<Dashboard />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/scenarios' element={<Scenarios />} />
           <Route path='/settings' element={<Settings />} />
--- src/components/scenario/scenario-toolbar.js
+++ src/components/scenario/scenario-toolbar.js
@@ -3,24 +3,46 @@ import React from "react";
 function ScenarioToolBar() {
   return (
     <>
-      <form class='overflow-hidden'>
-        <div class='row'>
-          <div class='col'>
+      <form className='overflow-hidden mb-3'>
+        <div className='row'>
+          <div className='col'>
             <input
               type='text'
-              class='form-control'
+              className='form-control'
               placeholder='Scenario name'
               aria-label='Scenario name'
             />
           </div>
-          <div class='col'>
+          <div className='col'>
             <input
               type='text'
-              class='form-control'
+              className='form-control'
               placeholder='Title'
               aria-label='Title'
             />
           </div>
+        </div>
+        <div className='row mt-2 justify-content-center'>
+          <button
+            type='button'
+            className='btn btn-outline-primary'
+            style={{width: "80px"}}>
+            Search
+          </button>
+        </div>
+        <div className='row mt-2 justify-content-end'>
+          <button
+            type='button'
+            className='btn btn-primary mr-2'
+            style={{width: "80px"}}>
+            Run
+          </button>
+          <button
+            type='button'
+            className='btn btn-primary mr-3'
+            style={{width: "80px"}}>
+            Create
+          </button>
         </div>
       </form>
     </>
